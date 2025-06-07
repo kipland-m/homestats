7dec368785f4fb570dd62a660186943cff706d2a
kipland-m <kiplandmelton@gmail.com>
2025-06-07 02:02:39 -0500

    setting up websocket server in interface

--- agent/agent.py
+++ agent/agent.py
@@ -1,5 +1,7 @@
 import requests
 import psutil
+import asyncio
+import websocket
 import time
 import os
 import time
@@ -8,27 +10,46 @@ import netifaces
 
 dotenv.load_dotenv()
 
+connected_clients = set()
 host = os.getenv("host")
 
-while True:
+async def handle_client(websocket, path):
+    connected_clients.add(websocket)
     try:
-        print(f"host ip: {host}")
+        async for message in websocket:
+            for client in connected_clients:
+                if client != websocket:
+                    await client.send(message)
 
-        print(netifaces.interfaces())
-        print(netifaces.interfaces(),netifaces.ifaddresses('lo'))
+    except websockets.exceptions.ConnectionClosed:
+        pass
 
-        hardware_data = {
-            'cpu_usage': psutil.cpu_percent(),
-            'memory_usage': psutil.virtual_memory().percent,
-            'cpu_freq': psutil.cpu_freq(),
-            'bytes_sent': psutil.net_io_counters().bytes_sent,
-            'bytes_received': psutil.net_io_counters().bytes_recv,
-        }
+    finally:
+        connected_clients.remove(websocket)
 
-        response = requests.post(f'http://{host}:5000/receive_hardware_data', json=hardware_data)
-        print(response.text)
-            
-        time.sleep(15)
+def main():
+    while True:
+        try:
+            print(f"host ip: {host}")
 
-    except Exception as e:
-        print("Error:", e)
+            print(netifaces.interfaces())
+            print(netifaces.interfaces(),netifaces.ifaddresses('lo'))
+
+            hardware_data = {
+                'cpu_usage': psutil.cpu_percent(),
+                'memory_usage': psutil.virtual_memory().percent,
+                'cpu_freq': psutil.cpu_freq(),
+                'bytes_sent': psutil.net_io_counters().bytes_sent,
+                'bytes_received': psutil.net_io_counters().bytes_recv,
+            }
+
+            response = requests.post(f'http://{host}:5000/receive_hardware_data', json=hardware_data)
+            print(response.text)
+                
+            time.sleep(15)
+
+        except Exception as e:
+            print("Error:", e)
+
+if __name__ == '__main__':
+    main()
